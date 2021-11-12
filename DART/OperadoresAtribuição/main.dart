7e604f648ee2e0d119a654759f01ecd9054e56bd
dougdalo <dougdalo@gmail.com>
2021-11-12 17:02:19 -0300

    AdicionaArquivos + MoveDados

---
+++ DART/OperadoresAtribuição/main.dart
@@ -0,0 +1,26 @@
+// declara a variável “a” e atribui o valor 1
+ // declara a variável “b” e não atribui valor
+ // o valor de “b” é nulo
+ var a = 1, b;
+
+ // se a variável “a” tiver valor nulo
+ // atribui o valor 3
+ a ??= 3;
+
+// se a variável “b” tiver valor nulo
+// atribui o valor 6
+b  ??= 6;
+
+print('o valor de a é ${a} e o valor de b é ${b}');
+
+// declara a variável x
+// e atribui o valor 10
+var x = 10;
+
+// somamos o valor 5
+// ao valor da variável x
+// e atribuímos o resultado
+//  a variável x
+x += 5;
+
+print('o valor de x é ${x}');
